97e10f726da8c3cdc4efb07b12608c2550b2b024
exozy <exozy.nebulous@gmail.com>
2024-12-18 15:37:04 +0500

    Clean Architecture

---
+++ newcli/CoreL/player.cs
@@ -0,0 +1,12 @@
+public class FPlayer{
+  public string Name {get; set;}
+  public string Surname {get; set;}
+  public string Sex {get; set;}
+  public string DateOfBrithday {get; set;}
+
+  public string Country {get; set;}
+}
+
+
+
+
